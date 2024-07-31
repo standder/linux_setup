b9e61b237b71b5f8ed938b0db22c55123a9116ff
standder <standder2@gmail.com>
2024-07-31 13:14:10 +0800

    first commit 2024.7.31

---
+++ install.sh
@@ -0,0 +1,10 @@
+## packer nvim setup
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+## nvim config file setup
+cp -r ./nvim ~/.config/
+
+## alacritty config file setup
+cp ./alacritty ~/.config/ -r
